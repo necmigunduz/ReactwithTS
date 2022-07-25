741475594907680bb7202808fffb40212b2cb79c
necmigunduz <necmigunduz@gmail.com>
2022-07-25 13:27:21 +0300

    Add fontawesome solid

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faArrowsToCircle } from '@fortawesome/free-solid-svg-icons'
 
 function App() {
   return (
     <div className="App">
       <h1 className="border bg-dark rounded text-white">Hello</h1>
+      <FontAwesomeIcon icon={faArrowsToCircle} size="2x"/>
     </div>
   );
 }
